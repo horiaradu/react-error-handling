19bc54a747b462d133ece961bd69e49fa1af83ab
Horia Radu <horia.radu@e-spres-oh.com>
2022-10-04 14:09:18 +0300

    generic handler only handles generic

--- src/genericErrorHandler.ts
+++ src/genericErrorHandler.ts
@@ -2,6 +2,9 @@ import { AxiosError } from 'axios';
 
 import { toast } from 'react-toastify';
 
-export function genericErrorHandler(error: AxiosError) {
-  toast('fail');
+export function genericErrorHandler(error: AxiosError<any>) {
+  if (error.response?.data?.error?.code === 'GENERIC_ERROR') {
+    console.error(error);
+    toast('fail');
+  }
 }
