4bc2976ffafc49c36db03a80d73dba3bdacf1eb7
Horia Radu <horia.radu@e-spres-oh.com>
2022-10-07 11:02:22 +0300

    Axios knows all errors we know

--- src/genericErrorHandler.ts
+++ src/genericErrorHandler.ts
@@ -2,8 +2,10 @@ import { AxiosError } from 'axios';
 
 import { toast } from 'react-toastify';
 
+const knownErrors = ['INVALID_TITLE'];
+
 export function genericErrorHandler(error: AxiosError<any>) {
-  if (error.response?.data?.error?.code === 'GENERIC_ERROR') {
+  if (!knownErrors.includes(error.response?.data?.error?.code)) {
     console.error(error);
     toast('fail');
   }
