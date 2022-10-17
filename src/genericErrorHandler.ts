636e635a961fd2aa2f7fde56b376221089f9cd3d
Horia Radu <horia.radu@e-spres-oh.com>
2022-10-17 16:23:03 +0300

    fix code

--- src/genericErrorHandler.ts
+++ src/genericErrorHandler.ts
@@ -2,6 +2,12 @@ import { AxiosError } from 'axios';
 
 import { toast } from 'react-toastify';
 
-export function genericErrorHandler(error: AxiosError) {
-  toast('fail');
+export function genericErrorHandler(error: AxiosError<any>) {
+  const code = error.response?.data?.error?.code;
+  console.error(error);
+  if (code === 'GENERIC_ERROR') {
+    toast('fail');
+  } else {
+    toast(code);
+  }
 }
